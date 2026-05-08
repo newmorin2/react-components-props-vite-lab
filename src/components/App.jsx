42615365911af2e25c08f1fb1d4793c41f0f97c1
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-08 15:05:33 +0300

    imported the components correctly and changed line 14

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import blogData from "../data/blog";
-
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Article from "./Article"
 console.log(blogData);
 
 function App() {
   return (
     <>
     <Header name= "Tech Blog"/>
-    <About about = "A tech blog about technologoy" />
-    <ArticleList />
-    <Article />
+    <About image = '/demo.png' about = "A tech blog about technology" />
+    <ArticleList posts = {blogData.posts} />
     </>
 
   );
