babd07e7a9f041df507ddd3fd58f106b1e2d1ba4
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-08 14:31:24 +0300

    created the other components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,13 @@ console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
+    <>
+    <Header name= "Newton's Life"/>
+    <About />
+    <ArticleList />
+    <Article />
+    </>
+
   );
 }
 
